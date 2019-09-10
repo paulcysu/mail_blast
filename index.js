d7c0c419c44ccb4fa0f67bbe2cb21a338d41b282
Paul <su.paul.cy@gmail.com>
2019-09-09 21:29:16 -0700

    restructured server

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+require('./services/passport');
+
 const app = express();
+require('./routes/authRoutes')(app);
 
-// http://localhost:5000/auth/google/callback (if wildcard error)
-
-passport.use(new GoogleStrategy({
-
-}))
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(process.env.PORT || 5000);
